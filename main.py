1c4e46cbfedd467e378d3bf289d94531dba7b38e
yuanhenglee <1990henrylee@gmail.com>
2022-05-03 16:07:32 +0800

    fix: tidy up

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import sys
 
+# return a group name : email mapping
 def getGroupDict( df ):
     group = {}
-
     for i in range(df.shape[0]):
         if df.iloc[i,0] == '組別名稱':
             name = df.iloc[i,1]
@@ -19,12 +19,14 @@ def getGroupDict( df ):
 
 
 def main():
+    # input args
     try:
         zuvio_path = sys.argv[1]
         webex_path = sys.argv[2]
     except:
         print("Usage: python3 main.py $ZUVIO_FILE $WEBEX_FILE")
         raise
+
     df=pd.read_excel(zuvio_path)
 
     output_data = []
@@ -32,6 +34,7 @@ def main():
         for email in v:
             output_data.append((k,email))
 
+    # output csv
     pd.DataFrame( output_data , columns = ['預先指定的分組討論名稱', '電子郵件地址']).to_csv('webex.csv', index = False, encoding = 'utf-8')
 
 if __name__ == '__main__':
